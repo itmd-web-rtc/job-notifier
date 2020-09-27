0edb5b4ebb7490d901007c9144b2a92dabddab84
dhirajj14 <djain14@hawk.iit.edu>
2020-09-27 10:49:44 -0500

    move routes and logic to controller

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,48 @@
+function homeController(){
+    return {
+        index(req, res){
+            const siteUrl = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=software&locT=C&locId=1128808&jobType=&context=Jobs&sc.keyword=Software+Engineer&dropdown=0";
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const fs = require('fs');
+
+//access the site
+const fetchData = async () => {
+  const result = await axios.get(siteUrl);
+  getData(result.data);
+};
+
+
+fetchData();
+
+
+//Scrape the data from response
+let getData = html => {
+  data = [];
+  const $ = cheerio.load(html);
+  $('.react-job-listing').each((i, elm) => {
+      data.push({
+        id: $(elm).attr('data-id'),
+        title: $(elm).find($('div > .jobHeader')).text(),
+        position: $(elm).find($('div > .jobInfoItem')).text(),
+        location: $(elm).find($('div > div >.loc')).text()
+      })
+  });
+  console.log(data);
+
+  // add data to file
+  fs.writeFile('Data.json', JSON.stringify(data),'utf8', (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+    // success case, the file was saved
+    console.log('Data Saved!');
+  });
+
+}
+// ----END---------------------------
+            res.render('./index');
+        }
+    }
+}
---
+++ routes/web.js
@@ -0,0 +1,11 @@
+function initRoutes(app){
+
+    const homeController = require('../app/http/controllers/homeController');
+   
+    //Route to index file
+    app.get('/', homeController().index);
+    
+    
+    }
+    
+    module.exports = initRoutes
