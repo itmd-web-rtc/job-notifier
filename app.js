67b065eeefe039e93a9a57d4421f7c2d05541a58
dhirajj14 <djain14@hawk.iit.edu>
2020-09-27 10:49:25 -0500

    setup directory structure

--- app.js
+++ app.js
@@ -1,56 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// import express
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!')
-})
+//get path
+const path = require('path');
 
-// This code is for testing the scraping must be restructured and organized
-// 
-// 
+// Initialize express
+const app = express();
 
-const siteUrl = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=software&locT=C&locId=1128808&jobType=&context=Jobs&sc.keyword=Software+Engineer&dropdown=0";
+//Get port from environment variable
+const PORT = process.env.PORT || 3000;
 
-const axios = require('axios');
-const cheerio = require('cheerio');
+//Assets
+app.use(express.static('public'));
 
-const fs = require('fs');
+//Set template engine
+app.use(expressLayout);
 
-//access the site
-const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  getData(result.data);
-};
+//set views path
+app.set('views', path.join(__dirname,'/resources/views'));
 
+//set app engine - ejs
+app.set('view engine', 'ejs');
 
-fetchData();
+//import web routes
+require('./routes/web')(app)
 
-
-//Scrape the data from response
-let getData = html => {
-  data = [];
-  const $ = cheerio.load(html);
-  $('.react-job-listing').each((i, elm) => {
-      data.push({
-        id: $(elm).attr('data-id'),
-        title: $(elm).find($('div > .jobHeader')).text(),
-        position: $(elm).find($('div > .jobInfoItem')).text(),
-        location: $(elm).find($('div > div >.loc')).text()
-      })
-  });
-  console.log(data);
-
-  // add data to file
-  fs.writeFile('Data.json', JSON.stringify(data),'utf8', (err) => {
-    // throws an error, you could also catch it here
-    if (err) throw err;
-    // success case, the file was saved
-    console.log('Data Saved!');
-  });
-
-}
-// ----END---------------------------
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+//Start server
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
