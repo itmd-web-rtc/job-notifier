0f696e71231e5e6ce47333ea155a3a807600b4b5
dhirajj14 <djain14@hawk.iit.edu>
2020-09-27 23:34:10 -0500

    move project from cheerio to pupeteer

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -2,49 +2,79 @@ function homeController() {
   return {
     index(req, res) {
 
-      const siteUrl = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=software&locT=C&locId=1128808&jobType=&context=Jobs&sc.keyword=Software+Engineer&dropdown=0";
+      const siteUrl = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=&locT=C&locId=1128808&jobType=&context=Jobs&sc.keyword=Software+Engineer&dropdown=0";
 
-      const axios = require('axios');
-      const cheerio = require('cheerio');
       const fs = require('fs');
 
-      data = [];
-      //access the site
-      const fetchData = async () => {
-        const result = await axios.get(siteUrl);
-        getData(result.data);
-      };
+      const puppeteer = require('puppeteer');
 
-      //Scrape the data from response
-      let getData = html => {
-        
-        const $ = cheerio.load(html);
-        $('.react-job-listing').each((i, elm) => {
+      (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(siteUrl);
+        data = [];
+        const id = await page.evaluate(() => Array.from(document.querySelectorAll('.react-job-listing'), element => element.getAttribute("data-id")));
+        const title = await page.evaluate(() => Array.from(document.querySelectorAll('.react-job-listing > div > .jobHeader'), element => element.innerText));
+        const pos = await page.evaluate(() => Array.from(document.querySelectorAll('.react-job-listing > div > .jobInfoItem'), element => element.innerText));
+        const loc = await page.evaluate(() => Array.from(document.querySelectorAll('.d-flex > .loc'), element => element.innerText));
+        for (var i = 0; i < id.length; i++) {
           data.push({
-            id: $(elm).attr('data-id'),
-            title: $(elm).find($('div > .jobHeader')).text(),
-            position: $(elm).find($('.jobInfoItem')).text(),
-            location: $(elm).find($('div >.loc')).text()
-          })
-        });
-        console.log(data);
-
+            id: id[i],
+            title: title[i],
+            position: pos[i],
+            location: loc[i]
+          });
+        }
+        console.log(data.sort((a, b) => parseInt(a.id) - parseInt(b.id)));
         // add data to file
         fs.writeFile('Data.json', JSON.stringify(data), 'utf8', (err) => {
           // throws an error, you could also catch it here
           if (err) throw err;
           // success case, the file was saved
           console.log('Data Saved!');
-          
-          res.render('index', {'jobData' : data});
-        });
 
-      }
+          res.render('index', { 'jobData': data });
+        });
+        await browser.close();
+      })();
+
+      
+      // //access the site
+      // const fetchData = async () => {
+      //   const result = await axios.get(siteUrl);
+      //   getData(result.data);
+      // };
+
+      // //Scrape the data from response
+      // let getData = html => {
+
+      //   const $ = cheerio.load(html);
+      //   $('.react-job-listing').each((i, elm) => {
+      //     data.push({
+      //       id: $(elm).attr('data-id'),
+      //       title: $(elm).find($('div > .jobHeader')).text(),
+      //       position: $(elm).find($('.jobInfoItem')).text(),
+      //       location: $(elm).find($('div >.loc')).text()
+      //     })
+      //   });
+      //   console.log(data);
+
+      //   // add data to file
+      //   fs.writeFile('Data.json', JSON.stringify(data), 'utf8', (err) => {
+      //     // throws an error, you could also catch it here
+      //     if (err) throw err;
+      //     // success case, the file was saved
+      //     console.log('Data Saved!');
+
+      //     res.render('index', {'jobData' : data});
+      //   });
+
+      // }
 
       //function fetch the data from website
-      fetchData();
-    
-      
+      // fetchData();
+
+
     }
   }
 }
